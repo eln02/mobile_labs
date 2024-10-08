6bfdcc65206fde84e0ede7139557f2ebb538d2dc
eln02 <drannikovaelena6@gmail.com>
2024-10-08 04:09:32 +0300

    Add lab 3 with all extras

--- app/src/main/java/com/example/mobile_labs/Laba3Activity.java
+++ app/src/main/java/com/example/mobile_labs/Laba3Activity.java
@@ -2,7 +2,10 @@ package com.example.mobile_labs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Laba3Activity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class Laba3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_laba3);
+
+        Button buttonLaba3 = findViewById(R.id.button);
+        buttonLaba3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Laba3Activity.this, Laba3_2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button buttonLaba3_1 = findViewById(R.id.button2);
+        buttonLaba3_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Laba3Activity.this, Laba3ScrollingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
